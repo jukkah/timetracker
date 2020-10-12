efbe3f1ace426388d97e93d22d052f19237df1f0
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2020-10-12 21:34:06 +0300

    fix(firestore): empty collection was never loaded

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -2,9 +2,13 @@ import { WithID } from "src/store/types";
 
 export const sanitize = <T>(value: T) => JSON.parse(JSON.stringify(value));
 
-export const objectToArray = <T>(data: { [key: string]: T }): undefined | WithID<T>[] => {
+export const objectToArray = <T>(data: { [key: string]: T } | undefined | null): undefined | WithID<T>[] => {
   if (data) {
     return Object.keys(data ?? {}).map(id =>({ id, ...data[id] }));
+  }
+
+  if (data === null) {
+    return [];
   }
 
   return undefined;
