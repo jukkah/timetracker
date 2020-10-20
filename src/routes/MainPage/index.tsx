d62addc43f8a425420563ea49dccfe2de1ae5a33
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2020-10-20 23:07:17 +0300

    feat(ui): enable swipe navigation between main page and list page

--- src/routes/MainPage/index.tsx
+++ src/routes/MainPage/index.tsx
@@ -5,8 +5,11 @@ import useCollection from 'src/hooks/useCollection';
 import { Record } from 'src/store/types';
 import cx from 'classnames';
 import { BsChevronCompactUp } from 'react-icons/bs';
+import { Swipeable } from 'react-swipeable';
+import { useHistory } from 'react-router-dom';
 
 const MainPage: React.FC = () => {
+  const history = useHistory();
   const firestore = useFirestore();
   const { documents, isLoading } = useCollection<Record>({ collection: 'records', orderBy: ['timestamp', 'desc'], limit: 1 });
   const inside = documents?.[0]?.type === 'in';
@@ -16,31 +19,33 @@ const MainPage: React.FC = () => {
   };
 
   return (
-    <div className="page" id="main-page">
-      <main>
-        <div className="status">
-          {isLoading ? 'Ladataan...' : inside ? 'Olet nyt sisällä' : 'Olet nyt ulkona'}
-        </div>
-        <div>
-          <button
-            onClick={() => change('in')}
-            className={cx('button call-to-action', { primary: !inside })}
-          >
-            Sisään
-          </button>
-          <button
-            onClick={() => change('out')}
-            className={cx('button call-to-action', { primary: inside })}
-          >
-            Ulos
-          </button>
-        </div>
-      </main>
-      <footer className="bottom">
-        <BsChevronCompactUp size={40} className="shake" />
-        Pyyhkäise ylös
-      </footer>
-    </div>
+    <Swipeable onSwipedUp={() => history.push('/records')}>
+      <div className="page" id="main-page">
+        <main>
+          <div className="status">
+            {isLoading ? 'Ladataan...' : inside ? 'Olet nyt sisällä' : 'Olet nyt ulkona'}
+          </div>
+          <div>
+            <button
+              onClick={() => change('in')}
+              className={cx('button call-to-action', { primary: !inside })}
+            >
+              Sisään
+            </button>
+            <button
+              onClick={() => change('out')}
+              className={cx('button call-to-action', { primary: inside })}
+            >
+              Ulos
+            </button>
+          </div>
+        </main>
+        <footer className="bottom">
+          <BsChevronCompactUp size={40} className="shake" />
+          Pyyhkäise ylös
+        </footer>
+      </div>
+    </Swipeable>
   );
 };
 
--- src/routes/RecordListPage/index.tsx
+++ src/routes/RecordListPage/index.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import RecordList from './RecordList';
 import { BsChevronCompactDown } from 'react-icons/bs';
+import { Swipeable } from 'react-swipeable';
 
 const RecordListPage: React.FC = () => {
+  const history = useHistory();
+  const main = useRef<HTMLElement | null>(null);
+  const [swipeEnabled, setSwipeEnabled] = useState(true);
+
   return (
-    <div className="page">
-      <div className="top">
-        <BsChevronCompactDown size={40} className="shake" />
+    <Swipeable
+      onSwipedDown={() => {
+        if (swipeEnabled) {
+          history.push('/');
+        }
+      }}
+      onSwiping={(e) => {
+        if (e.first) {
+          setSwipeEnabled((main.current?.getBoundingClientRect().top ?? 0) >= 0);
+        }
+      }}
+    >
+      <div className="page">
+        <div className="top">
+          <BsChevronCompactDown size={40} className="shake" />
+        </div>
+        <main ref={main}>
+          <RecordList />
+        </main>
+        <footer>
+          <Link to="/records/new" className="button">Lisää kirjaus</Link>
+        </footer>
       </div>
-      <main>
-        <RecordList />
-      </main>
-      <footer>
-        <Link to="/records/new" className="button">Lisää kirjaus</Link>
-      </footer>
-    </div>
+    </Swipeable>
   );
 };
 
